bcea6851cef61d0ecc56488fed752a12caf91afe
Ashish-kumar-Giri <ashishgiri1948@gmail.com>
2025-12-10 17:22:46 +0530

    Here is my 1st s3 bucket

---
+++ basics/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-west-1"
+  
+}
+resource "random_integer" "rand" {
+    min = 1000
+    max = 9999
+}
+resource "aws_s3_bucket" "demo" {
+  bucket = "tf-basics-${random_integer.rand.result}"
+  
+}
